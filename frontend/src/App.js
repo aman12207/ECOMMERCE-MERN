9ad9f795753066e0b299d49bf1156fc4d2baf32d
aman <amangupta12207@gmail.com>
2023-06-09 16:27:26 +0530

    Header and Footer Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import Header from "./component/layout/Header/Header";
+import WebFont from "webfontloader";
+import Footer from "./component/layout/Footer/Footer";
 
 function App() {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+        <Footer/>
+    </Router>
   );
 }
 
