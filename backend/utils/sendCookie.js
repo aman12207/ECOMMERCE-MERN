0924b478de7176436f02eb6264d2906334ee4a58
aman <amangupta12207@gmail.com>
2023-06-21 19:09:13 +0530

    changes acc to production

--- backend/utils/sendCookie.js
+++ backend/utils/sendCookie.js
@@ -1,16 +1,20 @@
-const sendCookie = (user,statusCode,res) =>{
+const sendCookie = (user, statusCode, res) => {
   const token = user.getJWTToken();
+
+  // options for cookie
   const options = {
-    expires : new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 24* 60 * 60 * 1000,
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    httpOnly : true
-  }
-  res.status(statusCode).cookie("token", token).json({
-    success:true,
+    path: '/', // Replace with your desired path
+    httpOnly: true,
+  };
+
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
     user,
-    token
-  })
-}
+    token,
+  });
+};
 
 module.exports = sendCookie
