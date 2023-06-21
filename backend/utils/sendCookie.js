fd4f7bf875ab4fb7398dce014cd3d89404ed91fd
aman <amangupta12207@gmail.com>
2023-06-21 20:05:02 +0530

    updated

--- backend/utils/sendCookie.js
+++ backend/utils/sendCookie.js
@@ -1,20 +1,16 @@
 const sendCookie = (user, statusCode, res) => {
   const token = user.getJWTToken();
-
-  // options for cookie
   const options = {
-    expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    path: '/', // Replace with your desired path
+    expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    sameSite: 'none', // Set sameSite to 'none' for cross-origin requests
+    secure: true, // Set secure to true for HTTPS
   };
-
-  res.status(statusCode).cookie("token", token, options).json({
+  res.status(statusCode).cookie('token', token, options).json({
     success: true,
     user,
     token,
   });
 };
 
-module.exports = sendCookie
+module.exports = sendCookie;
