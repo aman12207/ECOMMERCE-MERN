21ab1631672e1b94e87d88cfd32e40d833c31695
aman <amangupta12207@gmail.com>
2023-07-30 02:11:39 +0530

    vercel.json

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use('/api/v1', orderRoute);
 app.use('/api/v1', paymentRoute);
 
 // serving the frontend
-app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.use(express.static(path.join(__dirname, "./frontend/build")))
 
 app.get("*", (req, res) => {
     res.sendFile(
